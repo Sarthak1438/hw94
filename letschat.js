b4de059e59ff7885c4b580aca9e34c6517bf8e58
Sarthak1438 <80053025+Sarthak1438@users.noreply.github.com>
2021-12-27 21:52:25 +0530

    Add files via upload

---
+++ letschat.js
@@ -0,0 +1,13 @@
+//ADD YOUR FIREBASE LINKS
+var firebaseConfig = {
+    apiKey: "AIzaSyCQFUzZDjJ06osQA_NR8RdmqPQaDfd2ZE4",
+    authDomain: "projectc93-844fa.firebaseapp.com",
+    databaseURL: "https://projectc93-844fa-default-rtdb.firebaseio.com",
+    projectId: "projectc93-844fa",
+    storageBucket: "projectc93-844fa.appspot.com",
+    messagingSenderId: "812262660144",
+    appId: "1:812262660144:web:7ad34241da41c0ed9ab5f5"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig); 
